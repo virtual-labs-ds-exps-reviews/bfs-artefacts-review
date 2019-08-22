cbaa34998af8368b55eedd33b0af4e8b01887e02
sadhana <sadhana.virupaksha@gmail.com>
2019-08-22 17:33:11 +0530

    add code

---
+++ code/runtime/js/realization.js
@@ -0,0 +1,108 @@
+
+var realizationCatalog = {
+  "Pictorial Representation": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/project.png",
+    "width": "80%"
+  },
+
+  "Introduction": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/1qpnNjJzPFs"
+  },
+
+  "Queues Theory": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/ju0AKQ4ica8"
+  },
+
+  "Pictorial Representation of Queues": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/queue.png",
+    "width": "80%"
+  },
+	  
+   "Enqueue and Dequeue representation": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/queue.gif"
+  },
+
+  "Circular Queue implemented using Arrays": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/circular\ queue.gif"
+  },
+
+  "Undirected Graph": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/undirected.png",
+    "width": "80%"
+  },
+
+  "Directed Graph": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/directed.png",
+    "width": "80%"
+  },
+
+  "Pictorial Representation of Graph": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/graph.png",
+    "width": "80%"
+  },
+
+  "Graphs Introduction": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/C3v8SdK4Qyo"
+  },
+
+  "BFS and DFS Comparision": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/-nQOKN05a80"
+  },
+
+  "BFS using a Queue": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/bcfGjQq15PU"
+  },
+
+  "Queue demonstration": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/9rwCq9ZXMIY"
+  },
+
+  "BFS Demo-Graphs": {
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bfs_demo.html"
+  },
+
+  "BFSs": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/bfs.png",
+    "width": "80%"
+  },
+	
+  "DFSs": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/dfs.png",
+    "width": "80%"
+  },
+
+  "BFS Demo-Trees": {
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bfs_treedemo.html"
+  },
+
+  "BFS Practice-Graphs": {
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bfs_practice.html"
+  },
+
+  "BFS using Queue": {
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bfs_using_queue.html"
+  },
+  "BFS Exercise": {
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bfs_exercise.html"
+  }
+}
